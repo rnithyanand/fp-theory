b4fa73fa504714a2826cb572d35e104ef3102daf
rnithyanand <rnithyanand@cs.stonybrook.edu>
2013-09-12 15:34:47 -0400

    Script to execute initial supertrace computation.

---
+++ compute_st.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+rm -r 80*
+rm *.txt
+rm compute_ST
+rm core
+
+./clear_out.sh
+
+g++ -g -std=c++11 packets.cpp traces.cpp file_ops.cpp supertrace_helpers.cpp supertrace_func.cpp compute_ST.cpp -lm -o compute_ST
+
+num=$(awk 'BEGIN{for(i=1;i<=5;i+=0.25)print i}')
+
+
+for i in {80..120..20}
+do
+	for j in $num
+	do
+		for k in {1..10..1}
+		do
+			echo -e "\n Site: $k, Time Multiplier: $j, Trials: $i, Threshold: 80"
+			./compute_ST $k $i 80 $j 
+		done	
+	done
+	
+done
