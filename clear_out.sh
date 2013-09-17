35391e642043b5924fdebc3f60071b273bfe01f4
Rishab <rishabn@SGDP11.sgdp.cs.stonybrook.edu>
2013-09-17 12:54:24 -0400

    New script for computing supertrace on 6 core machines

--- clear_out.sh
+++ clear_out.sh
@@ -2,7 +2,7 @@
 clear
 
 echo "Clearing crumbs from last run"
-rm -r 80*
+#rm -r 80*
 rm core
 rm *.txt
 rm *.out
--- compute_st.sh
+++ compute_st.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-rm -r 80*
 rm *.txt
 rm compute_ST
 rm core
@@ -12,15 +11,25 @@ g++ -g -std=c++11 packets.cpp traces.cpp file_ops.cpp supertrace_helpers.cpp sup
 num=$(awk 'BEGIN{for(i=1;i<=5;i+=0.25)print i}')
 
 
-for i in {80..100..10}
-do
+#for i in {80..100..10}
+#do
 	for j in $num
 	do
 		for ((k = $1 ; k <= $2 ; k ++));
 		do
-			echo -e "\n Site: $k, Time Multiplier: $j, Trials: $i, Threshold: 80"
-			./compute_ST $k $i 80 $j 
+			echo -e "\n Site: $k, Time Multiplier: $j, Trials: 80, Threshold: 80"
+			./compute_ST $k 80 80 $j &
+			echo -e "\n Site: $k, Time Multiplier: $j, Trials: 90, Threshold: 80"
+			./compute_ST $k 90 80 $j &
+			echo -e "\n Site: $k, Time Multiplier: $j, Trials: 100, Threshold: 80"
+			./compute_ST $k 100 80 $j &
+			k=$((k+1))
+			echo -e "\n Site: $k, Time Multiplier: $j, Trials: 80, Threshold: 80"
+			./compute_ST $k 80 80 $j &
+			echo -e "\n Site: $k, Time Multiplier: $j, Trials: 90, Threshold: 80"
+			./compute_ST $k 90 80 $j &
+			echo -e "\n Site: $k, Time Multiplier: $j, Trials: 100, Threshold: 80"
+			./compute_ST $k 100 80 $j
 		done	
-	done
-	
-done
+	done	
+#done
