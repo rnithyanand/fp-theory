c7186d398d4687528506fec3b43cafc1077f3f0e
Rishab <rnithyanand@cs.stonybrook.edu>
2013-10-02 10:03:40 -0400

    STs for sites 991-1000

--- compute_st.sh
+++ compute_st.sh
@@ -6,7 +6,7 @@ rm core
 
 ./clear_out.sh
 
-g++ -g -std=c++11 -O3 packets.cpp traces.cpp file_ops.cpp supertrace_helpers.cpp supertrace_func.cpp compute_ST.cpp -lm -o compute_ST
+g++ -g -std=c++0x -O3 packets.cpp traces.cpp file_ops.cpp supertrace_helpers.cpp supertrace_func.cpp compute_ST.cpp -lm -o compute_ST
 
 #num=$(awk 'BEGIN{for(i=1;i<=5;i+=1)print i}')
 
@@ -27,38 +27,6 @@ for ((k = $1 ; k <= $2 ; k ++));
 		echo -e "\n Site: $k, Time Multiplier: $j, Trials: 90, Threshold: 80"
 		./compute_ST $k 100 90 1 &
 		echo -e "\n Site: $k, Time Multiplier: $j, Trials: 100, Threshold: 80"
-		./compute_ST $k 100 100 1 &
-
-		k=$((k+1))
-
-		echo -e "\n Site: $k, Time Multiplier: $j, Trials: 80, Threshold: 80"
-		./compute_ST $k 90 90 1 &
-		echo -e "\n Site: $k, Time Multiplier: $j, Trials: 90, Threshold: 80"
-		./compute_ST $k 100 90 1 &
-		echo -e "\n Site: $k, Time Multiplier: $j, Trials: 100, Threshold: 80"
-		./compute_ST $k 100 100 1 &
-
-		echo -e "\n Site: $k, Time Multiplier: $j, Trials: 80, Threshold: 80"
-		./compute_ST $k 80 80 1 &
-		echo -e "\n Site: $k, Time Multiplier: $j, Trials: 90, Threshold: 80"
-		./compute_ST $k 90 80 1 &
-		echo -e "\n Site: $k, Time Multiplier: $j, Trials: 100, Threshold: 80"
-		./compute_ST $k 100 80 1 &
-
-		k=$((k+1))
-
-		echo -e "\n Site: $k, Time Multiplier: $j, Trials: 80, Threshold: 80"
-		./compute_ST $k 90 90 1 &
-		echo -e "\n Site: $k, Time Multiplier: $j, Trials: 90, Threshold: 80"
-		./compute_ST $k 100 90 1 &
-		echo -e "\n Site: $k, Time Multiplier: $j, Trials: 100, Threshold: 80"
-		./compute_ST $k 100 100 1 &
-
-		echo -e "\n Site: $k, Time Multiplier: $j, Trials: 80, Threshold: 80"
-		./compute_ST $k 80 80 1 &
-		echo -e "\n Site: $k, Time Multiplier: $j, Trials: 90, Threshold: 80"
-		./compute_ST $k 90 80 1 &
-		echo -e "\n Site: $k, Time Multiplier: $j, Trials: 100, Threshold: 80"
-		./compute_ST $k 100 80 1
+		./compute_ST $k 100 100 1 
 	done	
 #done
