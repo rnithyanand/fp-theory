ac35acec0341e6403952d12888868cb597532232
Rishab <rnithyanand@cs.stonybrook.edu>
2013-10-09 00:08:45 -0400

    From Newton

--- compute_st_bw.sh
+++ compute_st_bw.sh
@@ -6,11 +6,11 @@ rm core
 g++ -g -std=c++0x -O3 packets.cpp traces.cpp file_ops.cpp supertrace_helpers.cpp supertrace_func.cpp compute_ST.cpp -lm -o compute_ST
 for ((k = $1 ; k <= $2 ; k ++));
 do
-        ./compute_ST $k 100 80 2 &
+        ./compute_ST $k 100 80 1.5 &
 	k=$((k+1))
-        ./compute_ST $k 100 80 2 &
+        ./compute_ST $k 100 80 1.5 &
         k=$((k+1))
-        ./compute_ST $k 100 80 2 &
+        ./compute_ST $k 100 80 1.5 &
         k=$((k+1))
-        ./compute_ST $k 100 80 2 
+        ./compute_ST $k 100 80 1.5
 done
