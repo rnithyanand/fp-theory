85f61fda199c9d0e2683a0e70361bd40516f18af
Rishab <rishabn@SGDP12.sgdp.cs.stonybrook.edu>
2013-10-13 18:49:08 -0400

    128 cluster script

--- compute_ht.sh
+++ compute_ht.sh
@@ -12,26 +12,24 @@ g++ -g -std=c++0x -O3 packets.cpp traces.cpp file_ops.cpp supertrace_helpers.cpp
 
 for(( k=$1 ; k <= $2 ; k++ ));
 do
-	./compute_HT $k 64 1 1 &
-	./compute_HT $k 64 2 1 &
-	./compute_HT $k 64 3 1 &
-	./compute_HT $k 64 4 1 &
-	./compute_HT $k 64 5 1 &
-	./compute_HT $k 64 6 1 
+	./compute_HT $k 128 1 1 &
+	./compute_HT $k 128 2 1 &
+	./compute_HT $k 128 3 1 &
+	./compute_HT $k 128 4 1 &
+	./compute_HT $k 128 5 1 &
+	./compute_HT $k 128 6 1 
 
-	./compute_HT $k 64 1 1.5 &
-	./compute_HT $k 64 2 1.5 &
-	./compute_HT $k 64 3 1.5 &
-	./compute_HT $k 64 4 1.5 &
-	./compute_HT $k 64 5 1.5 &
-	./compute_HT $k 64 6 1.5 
+	./compute_HT $k 128 1 1.5 &
+	./compute_HT $k 128 2 1.5 &
+	./compute_HT $k 128 3 1.5 &
+	./compute_HT $k 128 4 1.5 &
+	./compute_HT $k 128 5 1.5 &
+	./compute_HT $k 128 6 1.5 
 	
-	./compute_HT $k 64 1 2 &
-	./compute_HT $k 64 2 2 &
-	./compute_HT $k 64 3 2 &
-	./compute_HT $k 64 4 2 &
-	./compute_HT $k 64 5 2 &
-	./compute_HT $k 64 6 2 
-	
-
+	./compute_HT $k 128 1 2 &
+	./compute_HT $k 128 2 2 &
+	./compute_HT $k 128 3 2 &
+	./compute_HT $k 128 4 2 &
+	./compute_HT $k 128 5 2 &
+	./compute_HT $k 128 6 2 
 done
