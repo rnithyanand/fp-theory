04c2dfaee68169e6d85877df6aecb850a519dc17
Rishab <rishabn@SGDP11.sgdp.cs.stonybrook.edu>
2013-10-30 13:59:57 -0400

    Small Test

--- compute_st_bw.sh
+++ compute_st_bw.sh
@@ -6,35 +6,35 @@ rm core
 g++ -g -std=c++0x -O3 packets.cpp traces.cpp file_ops.cpp supertrace_helpers.cpp supertrace_func.cpp compute_ST.cpp -lm -o compute_ST
 for ((k = $1 ; k <= $2 ; k ++));
 do
-        ./compute_ST $k 50 40 .5 1 &
-        ./compute_ST $k 50 40 .65 1 &
-        ./compute_ST $k 50 40 .8 1 &
-	./compute_ST $k 50 40 .95 1
+        ./compute_ST $k 5 5 .5 1 &
+        ./compute_ST $k 5 5 .65 1 &
+        ./compute_ST $k 5 5 .8 1 &
+	./compute_ST $k 5 5 .95 1
 
-        ./compute_ST $k 50 40 .5 2 &
-        ./compute_ST $k 50 40 .65 2 &
-        ./compute_ST $k 50 40 .8 2 &
-	./compute_ST $k 50 40 .95 2
+        ./compute_ST $k 5 5 .5 2 &
+        ./compute_ST $k 5 5 .65 2 &
+        ./compute_ST $k 5 5 .8 2 &
+	./compute_ST $k 5 5 .95 2
         
-	./compute_ST $k 50 40 .5 3 &
-        ./compute_ST $k 50 40 .65 3 &
-        ./compute_ST $k 50 40 .8 3 &
-	./compute_ST $k 50 40 .95 3
+	./compute_ST $k 5 5 .5 3 &
+        ./compute_ST $k 5 5 .65 3 &
+        ./compute_ST $k 5 5 .8 3 &
+	./compute_ST $k 5 5 .95 3
 
         k=$((k+1))
 
-        ./compute_ST $k 50 40 .5 1 &
-        ./compute_ST $k 50 40 .65 1 &
-        ./compute_ST $k 50 40 .8 1 &
-	./compute_ST $k 50 40 .95 1
+        ./compute_ST $k 5 5 .5 1 &
+        ./compute_ST $k 5 5 .65 1 &
+        ./compute_ST $k 5 5 .8 1 &
+	./compute_ST $k 5 5 .95 1
 
-        ./compute_ST $k 50 40 .5 2 &
-        ./compute_ST $k 50 40 .65 2 &
-        ./compute_ST $k 50 40 .8 2 &
-	./compute_ST $k 50 40 .95 2
+        ./compute_ST $k 5 5 .5 2 &
+        ./compute_ST $k 5 5 .65 2 &
+        ./compute_ST $k 5 5 .8 2 &
+	./compute_ST $k 5 5 .95 2
         
-	./compute_ST $k 50 40 .5 3 &
-        ./compute_ST $k 50 40 .65 3 &
-        ./compute_ST $k 50 40 .8 3 &
-	./compute_ST $k 50 40 .95 3
+	./compute_ST $k 5 5 .5 3 &
+        ./compute_ST $k 5 5 .65 3 &
+        ./compute_ST $k 5 5 .8 3 &
+	./compute_ST $k 5 5 .95 3
 done
